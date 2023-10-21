acf3e2d63cad737373cf0c29dcb6c8ea76c3f1e0
Hanzla Saadi <hanzalasaadi@gmail.com>
2023-10-21 23:38:43 +0500

    added and changed footer

--- src/Home/Footer.js
+++ src/Home/Footer.js
@@ -1,14 +1,36 @@
 import React from "react";
+import "./HomeCss/footer.css";
+import { useNavigate } from "react-router-dom";
 const Footer = () => {
+  const nav = useNavigate();
   return (
     <>
       <footer className="style2">
-        <div className="container">
+        <div className="container footer">
+          <div className="footer-sec1">
+            <div className="first-div">
+              <img width={80} height={50} src="./logo.png" alt="Logo" />
+            </div>
+            <div className="second-div">
+              <p>Pakistan's leading chain of hotels and rooms.</p>
+            </div>
+            <div className="third-div">
+              <p style={{ textAlign: "end" }}>
+                Stay with us and make yourselves at home!
+              </p>
+            </div>
+            <div className="last-div">
+              <button onClick={() => nav("/City/1")}>
+                <i className="bi bi-house-door"></i>
+                See Our Rooms
+              </button>
+            </div>
+          </div>
           <div className="row border-top align-items-center">
             <div className="col-lg-6">
               <div className="copyright-area">
                 <p>
-                  © 2023 <b>LOFTYROOMS</b> is Proudly Powered by ----
+                  © 2023 <b>LOFTYROOMS</b> All Rights Reserved
                 </p>
               </div>
             </div>
